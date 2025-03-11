e6a38bad3d081007914a8e3010b82692f45040f4
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-03-11 23:52:36 +0100

    Moving assets folder inside source folder and updating linking CSS and JS paths

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "../assets/js/main.js";
-import "../assets/js/fixed.js";
-import "../assets/js/modal.js";
-import "../assets/css/index.css";
+import "./assets/js/main.js";
+import "./assets/js/fixed.js";
+import "./assets/js/modal.js";
+import "./assets/css/index.css";
