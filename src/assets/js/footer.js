6347f08dd67e3cd53e7224ee35ae10c8f9a5a05a
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-06-23 22:43:05 +0200

    Adding new skills to the Skillset section + ID names update and dynamic year in footer.

---
+++ src/assets/js/footer.js
@@ -0,0 +1,2 @@
+// Gets the current year and sets it as value in the footer text.
+document.getElementById("year").textContent = new Date().getFullYear();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./assets/js/main.js";
 import "./assets/js/fixed.js";
 import "./assets/js/modal.js";
+import "./assets/js/footer.js";
 import "./assets/css/index.css";
